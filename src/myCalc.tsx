ccaad448c93af02fd7e40e1f7dd41bab6a0f3253
Josiah Hands <josiahwhands@gmail.com>
2026-09-24 18:03:34 -0500

    style edit

--- src/myCalc.tsx
+++ src/myCalc.tsx
@@ -30,11 +30,11 @@ const MyCalc = () => {
                 <button onClick={() => handleClick("4")}>4</button>
                 <button onClick={() => handleClick("5")}>5</button>
                 <button onClick={() => handleClick("6")}>6</button>
-                <button onClick={handleCalc}>=</button>
+                <button onClick={() => handleClick(".")}>.</button>
                 <button onClick={() => handleClick("1")}>1</button>
                 <button onClick={() => handleClick("2")}>2</button>
                 <button onClick={() => handleClick("3")}>3</button>
-                <button onClick={() => handleClick(".")}>.</button>
+                <button onClick={handleCalc}>=</button>
                 <button id="zero" onClick={() => handleClick("0")}>0</button>
             </div>
         </div>
